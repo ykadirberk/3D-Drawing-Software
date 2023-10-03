79b89399fb5626d6c2df5ecaef3985ae30a27ba0
Kadir Berk Yağar <ykadirberk2@gmail.com>
2023-10-03 08:26:44 +0300

    Added GLM as project files

---
+++ 3D Drawing Software/include/Core/Shader/Shader.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#include <unordered_map>
+#include <string>
+
+#include "../../ext/glm/glm.hpp"
+#include "../DLLBuild.h"
+
+namespace dra {
+	
+	struct ShaderProgramSource {
+		std::string VertexSource;
+		std::string FragmentSource;
+	};
+
+	class _API Shader {
+		public:
+			Shader(const std::string& filepath);
+			~Shader();
+
+			void Bind() const;
+			void Unbind() const;
+			
+			void SetUniform1i(const std::string& name, int value);
+			void SetUniform1f(const std::string& name, float value);
+			void SetUniform4f(const std::string& name, float v0, float v1, float v2, float v3);
+			void SetUniformMat4f(const std::string& name, const glm::mat4& matrix);
+
+		private:
+			unsigned int m_RendererID;
+			std::string m_FilePath;
+			mutable std::unordered_map<std::string, int> m_UniformLocationCache;
+
+			ShaderProgramSource ParseShader(const std::string& filepath);
+			unsigned int CompileShader(unsigned int type, const std::string& source);
+			unsigned int CreateShader(const std::string& vertexShader, const std::string& fragmentShader);
+			int GetUniformLocation(const std::string& name) const;
+	};
+}
