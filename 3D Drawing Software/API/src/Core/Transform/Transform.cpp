9c95219f217affa5142ba82aa63ffbbdd61e1557
Kadir Berk Yağar <ykadirberk2@gmail.com>
2023-10-05 10:09:03 +0300

    update: Transform is contained in Object interface

--- 3D Drawing Software/API/src/Core/Transform/Transform.cpp
+++ 3D Drawing Software/API/src/Core/Transform/Transform.cpp
@@ -3,8 +3,15 @@
 #include "../../ext/glm/gtx/quaternion.hpp"
 #include "../../ext/glm/gtc/quaternion.hpp"
 
+#include "../Object/Object.h"
+
 namespace dra {
-	Transform::Transform() {
+	Transform::Transform() : m_Owner(nullptr) {
+		m_Position = glm::vec3(0.0f, 0.0f, 0.0f);
+		m_Rotation = glm::vec3(0.0f, 0.0f, 0.0f);
+		m_Scale = glm::vec3(1.0f, 1.0f, 1.0f);
+	}
+	Transform::Transform(Object* owner) : m_Owner(owner) {
 		m_Position	= glm::vec3(0.0f, 0.0f, 0.0f);
 		m_Rotation	= glm::vec3(0.0f, 0.0f, 0.0f);
 		m_Scale		= glm::vec3(1.0f, 1.0f, 1.0f);
@@ -13,27 +20,27 @@ namespace dra {
 	Transform::~Transform() {
 	}
 
-	void Transform::SetPosition(float x, float y, float z) {
+	void Transform::SetLocalPosition(float x, float y, float z) noexcept {
 		m_Position = glm::vec3(x, y, z);
 	}
 
-	void Transform::SetRotation(float x, float y, float z) {
+	void Transform::SetLocalRotation(float x, float y, float z) noexcept {
 		const float pi = std::acos(-1);
 		const float degree = pi / 180.0f;
 		m_Rotation = glm::vec3(degree * x, degree * y, degree * z);
 	}
 
-	void Transform::SetScale(float x, float y, float z) {
+	void Transform::SetLocalScale(float x, float y, float z) noexcept {
 		m_Scale = glm::vec3(x, y, z);
 	}
 
-	void Transform::Translate(float x, float y, float z) {
+	void Transform::Translate(float x, float y, float z) noexcept {
 		m_Position.x += x;
 		m_Position.y += y;
 		m_Position.z += z;
 	}
 
-	void Transform::Rotate(float x, float y, float z) {
+	void Transform::Rotate(float x, float y, float z) noexcept {
 		const float pi = std::acos(-1);
 		const float degree = pi / 180.0f;
 		m_Rotation.x = Interval(m_Rotation.x + (degree * x), 360.0f);
@@ -41,31 +48,93 @@ namespace dra {
 		m_Rotation.z = Interval(m_Rotation.z + (degree * z), 360.0f);
 	}
 
-	[[nodiscard]] glm::vec3 Transform::GetPosition() const noexcept {
+	[[nodiscard]] glm::vec3 Transform::GetLocalPosition() const noexcept {
 		return m_Position;
 	}
 
-	[[nodiscard]] glm::vec3 Transform::GetRotation() const noexcept {
+	[[nodiscard]] glm::vec3 Transform::GetLocalRotation() const noexcept {
 		return m_Rotation;
 	}
 
-	[[nodiscard]] glm::vec3 Transform::GetScale() const noexcept {
+	[[nodiscard]] glm::vec3 Transform::GetLocalScale() const noexcept {
 		return m_Scale;
 	}
 
-	[[nodiscard]] glm::mat4 Transform::GetAsMat4f() const noexcept {
-		glm::mat4 t_position_matrix = glm::translate(glm::mat4(1.0f), m_Position);
-		glm::mat4 t_rotation_matrix = glm::toMat4(glm::quat(m_Rotation));
-		glm::mat4 t_scale_matrix	= glm::scale(glm::mat4(1.0f), m_Scale);
-
-		return t_position_matrix * t_rotation_matrix * t_scale_matrix;
-	}
-	float Transform::Interval(float current, float max)
+	[[nodiscard]] glm::vec3 Transform::GetWorldPosition() const noexcept
 	{
+		glm::mat4 t = ParentOrientationMat4f(m_Owner);
+		return glm::vec3(t[3][0], t[3][1], t[3][2]);
+	}
+
+	[[nodiscard]] glm::vec3 Transform::GetWorldRotation() const noexcept {
+		//glm::vec
+		return ParentsRotationRecursiveVec3f(m_Owner);
+	}
+
+	[[nodiscard]] glm::vec3 Transform::GetWorldScale() const noexcept
+	{
+		return ParentsScaleRecursiveVec3f(m_Owner);
+	}
+
+	[[nodiscard]] glm::mat4 Transform::GetLocalAsMat4f() const noexcept {
+		return AsMat4f();
+	}
+
+	[[nodiscard]] glm::mat4 Transform::GetWorldAsMat4f() const noexcept {
+		return ParentOrientationMat4f(m_Owner);
+	}
+
+	[[nodiscard]] float Transform::Interval(float current, float max) const noexcept {
 		if (current < max) {
 			return current;
 		}
 		return Interval(current - max, max);
+	}
+
+	[[nodiscard]] glm::mat4 Transform::AsMat4f() const noexcept {
+		glm::mat4 t_position_matrix = glm::translate(glm::mat4(1.0f), m_Position);
+		glm::mat4 t_rotation_matrix = glm::toMat4(glm::quat(m_Rotation));
+		glm::mat4 t_scale_matrix = glm::scale(glm::mat4(1.0f), m_Scale);
+		return t_position_matrix * t_rotation_matrix * t_scale_matrix;
+	}
+	[[nodiscard]] glm::mat4 Transform::ParentOrientationMat4f(Object* obj) const noexcept {
+		if (!obj) {
+			return glm::mat4(1.0f);
+		}
+		auto parent = obj->GetParent();
+		auto transform = obj->GetTransform();
+
+
+		return ParentOrientationMat4f(parent) * transform.GetLocalAsMat4f();
+	}
+
+	[[nodiscard]] glm::vec3 Transform::ParentsPositionRecursiveVec3f(Object* obj) const noexcept {
+		if (!obj) {
+			return glm::vec3(1.0f);
+		}
+		auto parent = obj->GetParent();
+		auto transform = obj->GetTransform();
+		return ParentsPositionRecursiveVec3f(parent) + transform.GetLocalPosition();
+	}
+
+	glm::vec3 Transform::ParentsRotationRecursiveVec3f(Object* obj) const noexcept
+	{
+		if (!obj) {
+			return glm::vec3(1.0f);
+		}
+		auto parent = obj->GetParent();
+		auto transform = obj->GetTransform();
+		return ParentsRotationRecursiveVec3f(parent) + transform.GetLocalRotation();
+	}
+
+	glm::vec3 Transform::ParentsScaleRecursiveVec3f(Object* obj) const noexcept
+	{
+		if (!obj) {
+			return glm::vec3(1.0f);
+		}
+		auto parent = obj->GetParent();
+		auto transform = obj->GetTransform();
+		return ParentsScaleRecursiveVec3f(parent) * transform.GetLocalScale();
 	}
 }
 
--- 3D Drawing Software/API/src/Core/Transform/Transform.h
+++ 3D Drawing Software/API/src/Core/Transform/Transform.h
@@ -1,33 +1,52 @@
 #pragma once
 
-#include "../DLLBuild.h"
+#include <memory>
 
+#include "../DLLBuild.h"
 #include "../../ext/glm/glm.hpp"
 
 namespace dra {
+
+	class _API Object;
+
 	class _API Transform {
 		public:
 			Transform();
+			Transform(Object* owner);
 			~Transform();
 
-			void SetPosition(float x, float y, float z);
-			void SetRotation(float x, float y, float z);
-			void SetScale(float x, float y, float z);
+			void SetLocalPosition(float x, float y, float z) noexcept;
+			void SetLocalRotation(float x, float y, float z) noexcept;
+			void SetLocalScale(float x, float y, float z) noexcept;
 
-			void Translate(float x, float y, float z);
-			void Rotate(float x, float y, float z);
+			void Translate(float x, float y, float z) noexcept;
+			void Rotate(float x, float y, float z) noexcept;
 
-			[[nodiscard]] glm::vec3 GetPosition() const noexcept;
-			[[nodiscard]] glm::vec3 GetRotation() const noexcept;
-			[[nodiscard]] glm::vec3 GetScale() const noexcept;
+			[[nodiscard]] glm::vec3 GetLocalPosition() const noexcept;
+			[[nodiscard]] glm::vec3 GetLocalRotation() const noexcept;
+			[[nodiscard]] glm::vec3 GetLocalScale() const noexcept;
 
-			[[nodiscard]] glm::mat4 GetAsMat4f() const noexcept;
+			[[nodiscard]] glm::vec3 GetWorldPosition() const noexcept;
+			[[nodiscard]] glm::vec3 GetWorldRotation() const noexcept;
+			[[nodiscard]] glm::vec3 GetWorldScale() const noexcept;
 
+			[[nodiscard]] glm::mat4 GetLocalAsMat4f() const noexcept;
+			[[nodiscard]] glm::mat4 GetWorldAsMat4f() const noexcept;
+
+
+			[[nodiscard]] glm::vec3 ParentsPositionRecursiveVec3f(Object* obj) const noexcept;
+			[[nodiscard]] glm::vec3 ParentsRotationRecursiveVec3f(Object* obj) const noexcept;
+			[[nodiscard]] glm::vec3 ParentsScaleRecursiveVec3f(Object* obj) const noexcept;
 		private:
+			Object* m_Owner;
+
 			glm::vec3 m_Position;
 			glm::vec3 m_Rotation;
 			glm::vec3 m_Scale;
 
-			float Interval(float current, float max);
+			[[nodiscard]] float Interval(float current, float max) const noexcept;
+			[[nodiscard]] glm::mat4 AsMat4f() const noexcept;
+			[[nodiscard]] glm::mat4 ParentOrientationMat4f(Object* obj) const noexcept;
+			
 	};
 }
