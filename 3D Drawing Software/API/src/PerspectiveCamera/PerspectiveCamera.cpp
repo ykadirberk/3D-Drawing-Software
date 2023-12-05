a4e69c1eda990bbc4700c4a1aa2a7c94b606b2d7
Samet CAN <cansamet1223@gmail.com>
2023-12-05 09:07:09 +0300

    Little changes

--- 3D Drawing Software/API/src/PerspectiveCamera/PerspectiveCamera.cpp
+++ 3D Drawing Software/API/src/PerspectiveCamera/PerspectiveCamera.cpp
@@ -6,13 +6,6 @@ namespace dra {
 	: m_Fov(field_of_view), m_Width(width), m_Height(height), Camera(parent)
 	{
 		m_Projection = glm::perspective(glm::radians(m_Fov), m_Width / m_Height, 0.1f, 100.0f);
-		for (int row = 0; row < 4; row++) {
-			std::cout << "| ";
-			for (int column = 0; column < 4; column++) {
-				printf("%06.4f ", m_Projection[column][row]);
-			}
-			std::cout << "|" << std::endl;
-		}
 	}
 
 	PerspectiveCamera::~PerspectiveCamera() {
